0e9db220cdd66c1850cb0abd96f224e5755dafee
Ryan McKinley <ryantxu@gmail.com>
2024-01-12 18:06:41 -0800

    lint fix

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-import { DataQuery, DataSourceJsonData } from '@grafana/data';
+import { DataSourceJsonData } from '@grafana/data';
+import { DataQuery } from '@grafana/schema';
 
 export enum TCProduct {
   water_level = 'water_level',
